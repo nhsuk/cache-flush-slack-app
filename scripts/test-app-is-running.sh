3515ad5304ea5bcb9045c31e67b32e507e4d93aa
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2019-11-07 16:40:54 +0000

    :heavy_check_mark: Test http endpoint for app deployment

--- scripts/test-app-is-running.sh
+++ scripts/test-app-is-running.sh
@@ -10,13 +10,14 @@ APP_SERVICE_URL=$APP_SERVICE_HOST/api/slash-start
 # Use the env var set from the previous task with the path to the function app
 echo "Going to make request to function app: '$APP_SERVICE_URL'. "
 
-HTTP_STATUS=$(curl -sS -o /dev/null -w "%{http_code}" -XPOST "$APP_SERVICE_HOST?code=$FUNCTION_KEY")
+HTTP_STATUS=$(curl -sS -o /dev/null -w "%{http_code}" -XPOST "$APP_SERVICE_URL?code=$FUNCTION_KEY" -d "")
 
+EXPECTED_STATUS=204
 # This is a basic test to check the function app has been deployed. If the
 # deployment has failed no site exists.
-if [ "$HTTP_STATUS" = "204" ]; then
+if [ "$HTTP_STATUS" = "$EXPECTED_STATUS" ]; then
   echo "Got status '$HTTP_STATUS'. The app has been deployed successfully."
 else
-  echo "HTTP Status code was not 200. It looks like the app has not been deployed successfully."
+  echo "Got status '$HTTP_STATUS', was expecting '$EXPECTED_STATUS'. The app has not been deployed successfully."
   exit 1
 fi
