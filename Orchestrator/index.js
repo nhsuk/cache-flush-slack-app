1abd8407986294968f689c8efde5e0924a0fc24b
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2019-11-07 16:29:22 +0000

    :eyes: Rename for clarity and better URLs

--- Orchestrator/index.js
+++ Orchestrator/index.js
@@ -1,12 +1,12 @@
 const df = require('durable-functions');
 const qs = require('querystring');
 
-module.exports = df.orchestrator(function* orchestratorFunctionName(context) {
+module.exports = df.orchestrator(function* mainOrchestrator(context) {
   // context.log('******************ORCHESTRATION START');
   const input = context.df.getInput();
   const payload = JSON.parse(qs.unescape(qs.parse(input).payload));
 
-  const responseView = yield context.df.callActivity('CacheFlushRequest', payload);
-  yield context.df.callActivity('CacheFlushResponseActions', { view: responseView, viewId: payload.view.id });
+  const responseView = yield context.df.callActivity('cache-flush-request', payload);
+  yield context.df.callActivity('cache-flush-response-actions', { view: responseView, viewId: payload.view.id });
   // TODO: Send DM with the info
 });
