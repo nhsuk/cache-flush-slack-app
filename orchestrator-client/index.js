49a59485670f711a36c4916e5151708a64556585
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2019-11-09 15:22:19 +0000

    :heavy_check_mark: Verify request originated from Slack
    Verify requests to `/api/slash-start` originated from Slack (or a client
    that has access to the the signing secret at any rate!).

--- orchestrator-client/index.js
+++ orchestrator-client/index.js
@@ -3,6 +3,8 @@ const processingView = require('./views/processing.json');
 
 module.exports = async function orchestrationClient(context, req) {
   const client = df.getClient(context);
+  // TODO: Verify request is from Slack
+  context.log.error(req);
   const instanceId = await client.startNew('orchestrator-main', undefined, req.body);
   context.log(`Orchestration started with ID: ${instanceId}.`);
 
--- slash-start/index.js
+++ slash-start/index.js
@@ -1,31 +1,40 @@
 const qs = require('querystring');
 const rp = require('request-promise-native');
+const isRequestValid = require('../lib/verifyRequest');
 
 const openView = require('./views/open.json');
 
 module.exports = async function slashStart(context, req) {
-  openView.blocks[1].element.initial_value = qs.parse(req.body).text;
-
-  try {
-    const res = await rp({
-      body: {
-        trigger_id: qs.decode(req.body).trigger_id,
-        view: openView,
-      },
-      headers: {
-        Authorization: `Bearer ${process.env.SLACK_BOT_USER_OAUTH_ACCESS_TOKEN}`,
-        'Content-Type': 'application/json; charset=utf-8',
-      },
-      json: true,
-      method: 'POST',
-      url: 'https://slack.com/api/views.open',
-    });
-    if (!res.ok) {
-      context.log.error('Error processing views.open');
-      context.log.error(res);
+  if (isRequestValid(req)) {
+    openView.blocks[1].element.initial_value = qs.parse(req.body).text;
+    try {
+      const res = await rp({
+        body: {
+          trigger_id: qs.decode(req.body).trigger_id,
+          view: openView,
+        },
+        headers: {
+          Authorization: `Bearer ${process.env.SLACK_BOT_USER_OAUTH_ACCESS_TOKEN}`,
+          'Content-Type': 'application/json; charset=utf-8',
+        },
+        json: true,
+        method: 'POST',
+        url: 'https://slack.com/api/views.open',
+      });
+      if (!res.ok) {
+        context.log.error('Error processing views.open');
+        context.log.error(res);
+      }
+    } catch (err) {
+      context.log.error('Error occurred sending views.open');
+      context.log.error(err);
     }
-  } catch (err) {
-    context.log.error('Error occurred sending views.open');
-    context.log.error(err);
+    return null;
   }
+  context.log.error('Request signature did not match expected signature.');
+  return {
+    body: { message: 'Request signature did not match expected signature.' },
+    headers: { 'Content-Type': 'application/json' },
+    status: 401,
+  };
 };
