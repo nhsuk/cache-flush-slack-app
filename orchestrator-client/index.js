49c45f49254611ec17fbec75248a885f65e6fbfd
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2019-11-09 19:46:41 +0000

    :cop: Verify request originated from Slack
    Verify requests to `/api/orchestrator-client` originated from Slack (or a client
    that has access to the the signing secret at any rate!).

--- orchestrator-client/index.js
+++ orchestrator-client/index.js
@@ -1,17 +1,24 @@
 const df = require('durable-functions');
+const isRequestValid = require('../lib/verifyRequest');
 const processingView = require('./views/processing.json');
+const unauthorizedView = require('./views/unauthorized.json');
 
 module.exports = async function orchestrationClient(context, req) {
-  const client = df.getClient(context);
-  // TODO: Verify request is from Slack
-  context.log.error(req);
-  const instanceId = await client.startNew('orchestrator-main', undefined, req.body);
-  context.log(`Orchestration started with ID: ${instanceId}.`);
+  let view;
+  if (isRequestValid(req)) {
+    const client = df.getClient(context);
+    const instanceId = await client.startNew('orchestrator-main', undefined, req.body);
+    context.log(`Orchestration started with ID: ${instanceId}.`);
+    view = processingView;
+  } else {
+    context.log.error('Request signature did not match expected signature.');
+    view = unauthorizedView;
+  }
 
   return {
     body: {
       response_action: 'update',
-      view: processingView,
+      view,
     },
     headers: { 'Content-Type': 'application/json' },
     status: 200,
