3ae61bf714fcb047f9d4a9e597518c2a0fc0d734
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2019-11-24 18:24:05 +0000

    :hammer: Further simplification of message construction

--- verify-user-failed/index.js
+++ verify-user-failed/index.js
@@ -1,12 +1,18 @@
 /* eslint-disable camelcase */
 const qs = require('querystring');
-const verifyUserFailedMsg = require('../lib/verifyUserFailedMsg');
+const blocksBuilder = require('../lib/blocksBuilder');
+const { notAllowedMsg } = require('../lib/messages');
 
 module.exports = async function verifyRequestFailed(context, body) {
   const {
     team_domain, team_id, user_id, user_name,
   } = qs.parse(body);
+
   context.log.warn(`user_name '${user_name}' with user_id '${user_id}' in team_domain '${team_domain}' with team_id '${team_id}' is not on the allowed users list.`);
   context.log.error('Request signature did not match expected signature');
-  return verifyUserFailedMsg(user_id, user_name);
+
+  return {
+    blocks: blocksBuilder(notAllowedMsg(user_id, user_name)),
+    text: 'User not allowed',
+  };
 };
